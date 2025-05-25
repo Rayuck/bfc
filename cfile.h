95e28397aabbf73151dbfef2179e95f9214b6d1f
Rayuck <jacktheamazinghd@gmail.com>
2025-05-25 15:18:20 +0100

    Fixed issue with pointers in output file being handles incorrectly

--- cfile.h
+++ cfile.h
@@ -2,35 +2,38 @@
 
 void initCFile(FILE *file){
     fprintf(file, "#include <stdio.h>\n#include <stdlib.h>\n");
-    fprintf(file, "#include <stdint.h>\n");
     fprintf(file, "int main(int argc, char **argv){\n");
     fprintf(file, "++argv;\n");
-    fprintf(file, "uint8_t *cells=malloc(30000);\n");
+    fprintf(file, "char *cells=malloc(30000);\n");
+    fprintf(file, "int cell_ptr=0;\n");
 }
 
 void finalizeCFile(FILE *file){
     fprintf(file, "cells=0;\n");
     fprintf(file, "free(cells);\n");
+    fprintf(file, "return 0;\n");
     fprintf(file, "}");
 }
 
 void inc(FILE *file,int value){
-    fprintf(file, "*cells+=%i;\n",value);
+    fprintf(file, "*(cells+cell_ptr)+=%i;\n",value);
 }
 
 void dec(FILE *file,int value){
-    fprintf(file, "*cells-=%i;\n",value);
+    fprintf(file, "*(cells+cell_ptr)-=%i;\n",value);
 }
 
 void mvr(FILE *file,int value){
-    fprintf(file, "cells+=%i;\n",value);
+    fprintf(file, "if(cell_ptr==29999){cell_ptr=0;}");
+    fprintf(file, "else{cell_ptr+=%i;}\n",value);
 }
 
 void mvl(FILE *file,int value){
-    fprintf(file, "cells-=%i;\n",value);
+    fprintf(file, "if(cell_ptr==0){cell_ptr=29999;}");
+    fprintf(file, "else{cell_ptr-=%i;}\n",value);
 }
 
 void handle_input(FILE *file){
-    fprintf(file, "if(**argv==0){*(*argv)--;*cells=**argv;}\n");
-    fprintf(file, "else{*cells=**argv;*(*argv)++;}\n");
+    fprintf(file, "if(**argv==0){*(*argv)--;*(cells+cell_ptr)=**argv;}\n");
+    fprintf(file, "else{*(cells+cell_ptr)=**argv;*(*argv)++;}\n");
 }
--- main.c
+++ main.c
@@ -14,142 +14,39 @@ int main(int argc, char **argv){
     FILE *f = fopen(argv[1], "r");
     FILE *outFile = fopen(argv[2], "w");
     initCFile(outFile);
-    
-
-    char curchar;
-    int fsize=0;
 
     uint32_t count = 0;
     uint8_t shouldExit = 0;
-
-    // init f & reset pointer offset //
-    fgetc(f);
-    f->_IO_read_ptr--;
+    char curchar=1;
     
-    while(*f->_IO_read_ptr!=0){
-        switch(*f->_IO_read_ptr){
+    while(curchar!=EOF){
+	curchar = fgetc(f);
+        switch(curchar){
             case '+':
-                ++f->_IO_read_ptr;
-                count = 1;
-                shouldExit=0;
-                while(shouldExit==0){
-                    switch(*f->_IO_read_ptr){
-                        case '+':
-                            ++count;
-                            ++f->_IO_read_ptr;
-                            break;
-                        case ' ':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\n':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\t':
-                            ++f->_IO_read_ptr;
-                            break;
-                        default:
-                            shouldExit=1;
-                            break;
-                    }
-                }
-                inc(outFile, count);
-                --f->_IO_read_ptr;
+                inc(outFile, 1);
                 break;
             case '-':
-                ++f->_IO_read_ptr;
-                count = 1;
-                shouldExit=0;
-                while(shouldExit==0){
-                    switch(*f->_IO_read_ptr){
-                        case '-':
-                            ++count;
-                            ++f->_IO_read_ptr;
-                            break;
-                        case ' ':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\n':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\t':
-                            ++f->_IO_read_ptr;
-                            break;
-                        default:
-                            shouldExit=1;
-                            break;
-                    }
-                }
-                dec(outFile, count);
-                --f->_IO_read_ptr;
+                dec(outFile, 1);
                 break;
             case '>':
-                ++f->_IO_read_ptr;
-                count = 1;
-                shouldExit=0;
-                while(shouldExit==0){
-                    switch(*f->_IO_read_ptr){
-                        case '>':
-                            ++count;
-                            ++f->_IO_read_ptr;
-                            break;
-                        case ' ':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\n':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\t':
-                            ++f->_IO_read_ptr;
-                            break;
-                        default:
-                            shouldExit=1;
-                            break;
-                    }
-                }
-            mvr(outFile, count);
-                --f->_IO_read_ptr;
+                mvr(outFile, 1);
                 break;
             case '<':
-                ++f->_IO_read_ptr;
-                count = 1;
-                shouldExit=0;
-                while(shouldExit==0){
-                    switch(*f->_IO_read_ptr){
-                        case '<':
-                            ++count;
-                            ++f->_IO_read_ptr;
-                            break;
-                        case ' ':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\n':
-                            ++f->_IO_read_ptr;
-                            break;
-                        case '\t':
-                            ++f->_IO_read_ptr;
-                            break;
-                        default:
-                            shouldExit=1;
-                            break;
-                    }
-                }
-                mvl(outFile, count);
-                --f->_IO_read_ptr;
+                mvl(outFile, 1);
                 break;
             case '[':
-                fprintf(outFile, "while(*cells!=0){\n");
+                fprintf(outFile, "while(*(cells+cell_ptr)!=0){\n");
                 break;
             case ']':
                 fprintf(outFile, "}\n");
                 break;
             case '.':
-                fprintf(outFile, "putc(*cells, stdout);\n");
+                fprintf(outFile, "putc(*(cells+cell_ptr), stdout);\n");
                 break;
             case ',':
                 handle_input(outFile);
                 break;
         }
-        ++f->_IO_read_ptr;
     }
 
     finalizeCFile(outFile);
